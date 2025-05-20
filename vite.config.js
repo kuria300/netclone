42c8fac712adcbb258c3c3b2b78f2df5155d0400
kuria300 <eugenekuria66@gmail.com>
2025-05-20 22:32:17 +0300

    vite.congig.js commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // in KB, increase from 500 to 1000
+  },
 })
+
