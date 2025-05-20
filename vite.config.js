a9c05e6d622eef22c6e0c8dcedee274fffff2b50
kuria300 <eugenekuria66@gmail.com>
2025-05-20 22:54:22 +0300

    Apply fixes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // in KB, increase from 500 to 1000
+    chunkSizeWarningLimit: 1000, 
   },
 })
 
